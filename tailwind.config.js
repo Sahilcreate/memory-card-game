7b243364acfca0744d2580b6b542bbc7b732bd10
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2025-01-05 16:32:00 +0530

    feat: Add custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        //cards-section grid
+        "max-content-6": "repeat(6, minmax(0, max-content))",
+        "max-content-3": "repeat(3, minmax(0, max-content))",
+        "max-content-2": "repeat(2, minmax(0, max-content))",
+      },
+      backgroundImage: {
+        "custom-gradient": "linear-gradient(to bottom, #F2C94C, #F2994A)",
+      },
+    },
   },
   plugins: [],
 };
